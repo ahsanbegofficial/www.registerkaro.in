1929f220cd3fc01658c3354816074fcdcaf16e14
ahsanofficial <bega@gmail.com>
2024-02-19 19:09:06 +0530

    add ip for vercell

--- pages/api/ip.js
+++ pages/api/ip.js
@@ -1,5 +1,12 @@
-export default function handler(req, res) {
-    const ip = req.connection.remoteAddress;
-    console.log(ip);
-    res.status(200).json({ ip });
+import axios from 'axios';
+
+export default async function handler(req, res) {
+    try {
+        const response = await axios.get('https://api.ipify.org?format=json');
+        const serverIP = response.data.ip;
+        res.status(200).json({ serverIP });
+    } catch (error) {
+        console.error('Error retrieving server IP:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
