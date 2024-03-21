38987bd12a0895de9aca5c84b8eb57edbc99af6e
ahsanofficial <bega@gmail.com>
2024-03-21 17:28:51 +0530

    remove clarity

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,15 +33,6 @@ export default function App({ Component, pageProps, router }: AppProps) {
         {shouldRenderfooter && <Footer />}
         {shouldRenderZohoSalesIQ && <ZohoSalesIQ />}
       </SessionProvider>
-      <Script id="ms-clarity" dangerouslySetInnerHTML={{
-        __html: `
-        (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "kl3azert5x");
-          `,
-      }} />
       <Script id="google-tag-script" dangerouslySetInnerHTML={{
         __html: `
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
